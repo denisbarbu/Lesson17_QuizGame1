20472d3cc709aead8d3ea97f49fbf63fa664a114
Denis <barbudenisphoto@gmail.com>
2024-03-25 10:28:51 -0700

    added login feature to a program and also new user

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+# create a quiz game with admin and players. A user has to login.
+#if player, then he can play, if admin can add questions
+
+import users
+
+
+
+
+if __name__ == '__main__':
+    welcome_msg = "Welcome to Quiz Game"
+    print(f"{len(welcome_msg) * '='}{welcome_msg}{len(welcome_msg) * '='}")
+
+
+    users.login()
